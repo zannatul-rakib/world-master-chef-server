c46b17dd98ca9bc599f5f416667820fddcf4408e
zrakib-dev <dev.zrakib@gmail.com>
2023-06-22 03:50:02 +0600

    server data load

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require("cors");
+var chef = require("./data/chef.json");
+var recipes = require("./data/recipes.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("word master chef is running on server");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const result = chef.find((c) => c.id === id);
+  res.send(result);
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
